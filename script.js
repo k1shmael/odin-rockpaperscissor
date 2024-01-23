00167ec54912748cfa67a7896c136ba551d40b4b
Kevin Ishmael <kevinishmael91@gmail.com>
2024-01-22 19:41:31 -0500

    started

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+function getcomputerChoice() {
+ 
+
+return"rock"} 
+
+
+let userInput = prompt("Choose rock paper or scissors ");
+console.log(userInput);
+
+console.log (getcomputerChoice());
